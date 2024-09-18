e2433a9331ab45a6001fa07b15caf104286687bc
Kovács Richárd <157577505+Ricsko20@users.noreply.github.com>
2024-09-18 09:53:46 +0200

    Add files via upload

---
+++ feladat1.js
@@ -0,0 +1,7 @@
+function kiiras() {
+    let adatok = [];
+    adatok.push(document.getElementById("nev").value)
+    adatok.push(document.getElementById("kor").value)
+    adatok.push(document.getElementById("nem").value)
+    console.log(adatok)
+}
